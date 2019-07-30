82e6a11bd0b966e1f3a2e279fa1133c289b8d54e
Marco Reitano <marco.reitano@th-koeln.de>
2019-07-31 00:51:16 +0200

    Adds grided artist elements

--- catalogue-search-results.js
+++ catalogue-search-results.js
@@ -15,6 +15,43 @@ class CatalogueSearchResults extends PolymerElement {
       <link rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.5/css/bulma.min.css">
       <div class="search-container">
+        <div class="tile is-ancestor is-vertical">
+          <div class="tile">
+            <div class="tile is-parent">
+              <div class="tile is-child">
+                <catalogue-artist-element artist='{"alias":"Ed Sheeran", "genre":"Singer/Songwriter", "facebookuri":"http://www.facebook.com/9189674485"}'></catalogue-artist-element>
+              </div>
+            </div>
+            <div class="tile is-parent">
+              <div class="tile is-child">
+                <catalogue-artist-element artist='{"alias":"Ed Sheeran", "genre":"Singer/Songwriter", "facebookuri":"http://www.facebook.com/9189674485"}'></catalogue-artist-element>
+              </div>
+            </div>
+            <div class="tile is-parent">
+              <div class="tile is-child">
+                <catalogue-artist-element artist='{"alias":"Ed Sheeran", "genre":"Singer/Songwriter", "facebookuri":"http://www.facebook.com/9189674485"}'></catalogue-artist-element>
+              </div>
+            </div>
+          </div>  
+           
+          <div class="tile">
+            <div class="tile is-parent">
+              <div class="tile is-child">
+                <catalogue-artist-element artist='{"alias":"Ed Sheeran", "genre":"Singer/Songwriter", "facebookuri":"http://www.facebook.com/9189674485"}'></catalogue-artist-element>
+              </div>
+            </div>
+            <div class="tile is-parent">
+              <div class="tile is-child">
+                <catalogue-artist-element artist='{"alias":"Ed Sheeran", "genre":"Singer/Songwriter", "facebookuri":"http://www.facebook.com/9189674485"}'></catalogue-artist-element>
+              </div>
+            </div>
+            <div class="tile is-parent">
+              <div class="tile is-child">
+                <catalogue-artist-element artist='{"alias":"Ed Sheeran", "genre":"Singer/Songwriter", "facebookuri":"http://www.facebook.com/9189674485"}'></catalogue-artist-element>
+              </div>
+            </div>
+          </div>            
+      </div>
         <catalogue-artist-list></catalogue-artist-list>
       </div>
     `;
