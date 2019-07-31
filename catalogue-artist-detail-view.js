294b6faf671668eae4e721916d31e24aacbdcc36
Marco Reitano <marco.reitano@th-koeln.de>
2019-07-31 15:34:29 +0200

    try location.param access 8

--- catalogue-artist-detail-view.js
+++ catalogue-artist-detail-view.js
@@ -16,9 +16,7 @@ class ArtistDetailView extends PolymerElement {
       <style xmlns="http://www.w3.org/1999/html">
       </style>
       <div>
-        <a onclick="logLocation()">CLICK ME</a>
-        <p>[[location.param.id]]</p>
-        <p>[[location.params.0]]</p>
+        <p>[[this.location.param.id]]</p>
         <p>[[window.location.params.id]]</p>
         <p>[[window.location.params.0]]</p>
         <div class="artist-alias">[[artist.alias]]</div>
@@ -45,15 +43,8 @@ class ArtistDetailView extends PolymerElement {
     super.ready();
     console.log('Test1' + window.location.pathname);
     console.log('Test2' + this.location.params.id);
-    console.log('Test3' + this.location.params[0]);
     console.log('Test4' + location.params.id);
-    console.log('Test5' + location.params[0]);
     console.log('Test5' + this.location.pathname);
-  }
-
-  logLocation() {
-    console.log('something?!');
-    console.log('Test6' + window.location.pathname);
   }
 }
 
