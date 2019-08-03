f639f5f78d713fc12c83b69bb505e65d685c19e5
Marco Reitano <marco.reitano@th-koeln.de>
2019-08-03 23:05:23 +0200

    Pushstate/popstate

--- catalogue-home-view.js
+++ catalogue-home-view.js
@@ -36,10 +36,12 @@ class HomeView extends PolymerElement {
   }
 
   test() {
-    window.location.href = "/artist/123";
-    window.dispatchEvent(new CustomEvent(
-        `vaadin-router-go`, {cancelable: true, pathname: '/artist/123'}
-    ));
+    // window.location.href = "/artist/123";
+    // window.dispatchEvent(new CustomEvent(
+    //     `vaadin-router-go`, {cancelable: true, pathname: '/artist/123'}
+    // ));
+    window.history.pushState({}, null, '/artist/123');
+    window.dispatchEvent(new PopStateEvent('popstate'));
   }
 }
 
