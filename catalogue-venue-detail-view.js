5d46db7a479475fd81f77881645fa2f9d6f648d4
Marco Reitano <marco.reitano@th-koeln.de>
2019-08-10 19:28:40 +0200

    Adds concert detail view and venue detail view

---
+++ catalogue-venue-detail-view.js
@@ -0,0 +1,59 @@
+import {html, PolymerElement} from '@polymer/polymer/polymer-element.js';
+
+/**
+ * `catalogue-venue-detail-view`
+ *
+ *
+ * @customElement
+ * @polymer
+ * @demo demo/index.html
+ */
+class VenueDetailView extends PolymerElement {
+  static get template() {
+    return html`
+    <link rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.5/css/bulma.min.css">
+      <style xmlns="http://www.w3.org/1999/html">
+      </style>
+      <div>
+      venue detail view works!
+        <p>{{this.location.params.id}}</p>
+        <p>[[this.location.params.id]]</p>
+
+        <!--<app-indexeddb-mirror -->
+          <!--id="datastore"-->
+          <!--key="search">-->
+        <!--</app-indexeddb-mirror>-->
+      </div>
+    `;
+  };
+
+  // @formatter:off
+  static get importMeta() {
+    return import.meta;
+  }
+  // @formatter:on
+
+  static get properties() {
+    return {
+      venue: {
+        type: Object
+      },
+      venueData: {
+        type: Array
+      }
+    };
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+    console.log("connectedCallback");
+  }
+
+  ready() {
+    super.ready();
+    console.log("ready");
+  }
+}
+
+window.customElements.define('catalogue-venue-detail-view', VenueDetailView);
